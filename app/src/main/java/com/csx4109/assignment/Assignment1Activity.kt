f00ab7e32e19fdaa14f3c6f18fa182208d4d3b87
tchawengvora <thanathas.chawengvorakul@agoda.com>
2023-12-09 02:07:48 +0700

    Add first 2 assignment

--- app/src/main/java/com/csx4109/assignment/Assignment1Activity.kt
+++ app/src/main/java/com/csx4109/assignment/Assignment1Activity.kt
@@ -3,6 +3,24 @@ package com.csx4109.assignment
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * Page Name: Basic Calculator
+ *
+ * -- Description --
+ * Create an app can do basic calculation, consists of
+ * - textView named `tvResult`
+ * - button of number 0-9 named `btn0` - `btn9`
+ * - button named `btnPlus`
+ * - button named `btnEqual`
+ * - button named `btnClear`
+ *
+ * Cases
+ * - When open an application, the first value in `tvResult` should be 0
+ * - When type any number button and `btnPlus`, value on `tvResult` should be updated
+ * - When click `btnEqual`, value on `tvResult` should updated to be the result of your calculation
+ * - After click `btnEqual` to calculate the result, you should be able to type more number for further calculation
+ * - When click `btnClear`, value on `tvResult` should return to 0
+ */
 class Assignment1Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/csx4109/assignment/ExampleAssignmentActivity.kt
+++ app/src/main/java/com/csx4109/assignment/ExampleAssignmentActivity.kt
@@ -1,7 +1,24 @@
 package com.csx4109.assignment
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.csx4109.assignment.databinding.ActivityExampleAssignmentBinding
+
+/**
+ * Page Name: Counting Game
+ *
+ * -- Description --
+ * Create an app that has
+ * - textView named `tvCounter`
+ * - button named `btnPlus`
+ * - button named `btnMinus`
+ *
+ * Cases
+ * - When open an application, the first value in `tvCounter` should be 0
+ * - When click `btnPlus`, `tvCounter` should be increased by 1
+ * - When click `btnMinus`, `tvCounter` should be decreased by 1
+ */
 
 class ExampleAssignmentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
