0158c27cbc7c470bedf4b231b93e468b6cd3a284
thanathasCh <tamasama077@gmail.com>
2023-12-10 15:13:13 +0700

    Update test

--- app/src/androidTest/java/com/csx4109/assignment/ExampleAssignmentInstrumentedTest.kt
+++ app/src/androidTest/java/com/csx4109/assignment/ExampleAssignmentInstrumentedTest.kt
@@ -3,11 +3,11 @@
 //import androidx.test.core.app.ActivityScenario
 //import androidx.test.espresso.Espresso.onView
 //import androidx.test.espresso.action.ViewActions.click
+//import androidx.test.espresso.assertion.ViewAssertions.matches
 //import androidx.test.espresso.matcher.ViewMatchers.withText
 //import androidx.test.espresso.matcher.ViewMatchers.withId
 //import androidx.test.ext.junit.runners.AndroidJUnit4
 //import androidx.test.filters.LargeTest
-//import org.hamcrest.Matchers.allOf
 //import org.junit.Before
 //import org.junit.Test
 //import org.junit.runner.RunWith
@@ -24,12 +24,8 @@
 //
 //    @Test
 //    fun defaultValueShouldBe0() {
-//        onView(
-//            allOf(
-//                withId(R.id.tvCounter),
-//                withText("0")
-//            )
-//        )
+//        onView(withId(R.id.tvCounter))
+//            .check(matches(withText("0")))
 //    }
 //
 //    @Test
@@ -38,12 +34,8 @@
 //            onView(withId(R.id.btnPlus))
 //                .perform(click())
 //
-//            onView(
-//                allOf(
-//                    withId(R.id.tvCounter),
-//                    withText(i.toString())
-//                )
-//            )
+//            onView(withId(R.id.tvCounter))
+//                .check(matches(withText(i.toString())))
 //        }
 //    }
 //
@@ -53,12 +45,8 @@
 //            onView(withId(R.id.btnMinus))
 //                .perform(click())
 //
-//            onView(
-//                allOf(
-//                    withId(R.id.tvCounter),
-//                    withText(i.toString())
-//                )
-//            )
+//            onView(withId(R.id.tvCounter))
+//                .check(matches(withText(i.toString())))
 //        }
 //    }
 //}
