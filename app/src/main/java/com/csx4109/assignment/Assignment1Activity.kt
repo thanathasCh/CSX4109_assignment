d8211ded05ce2d1f08aca503a06d06883582a87d
Hein Thant <121175402+heian2704@users.noreply.github.com>
2023-12-15 20:41:47 +0700

    6430047 -assignment1

--- app/src/main/java/com/csx4109/assignment/Assignment1Activity.kt
+++ app/src/main/java/com/csx4109/assignment/Assignment1Activity.kt
@@ -2,6 +2,7 @@ package com.csx4109.assignment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.csx4109.assignment.databinding.ActivityAssignment1Binding
 
 /**
  * Page Name: Basic Calculator
@@ -22,8 +23,62 @@ import android.os.Bundle
  * - When click `btnClear`, value on `tvResult` should return to 0
  */
 class Assignment1Activity : AppCompatActivity() {
+    private val view: ActivityAssignment1Binding by lazy {
+        ActivityAssignment1Binding.inflate(
+            layoutInflater
+        )
+    }
+    private var currentValue = 0
+    private var storedValue = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_assignment1)
+        setContentView(view.root)
+
+        setNumberButtonListeners()
+        setPlusEqualButtonListeners()
+        setClearButtonListeners()
     }
+
+    private fun setClearButtonListeners() {
+        view.btnClear.setOnClickListener {
+            currentValue = 0
+            storedValue = 0
+            updateResult()
+        }
+    }
+
+    private fun setPlusEqualButtonListeners() {
+        view.btnPlus.setOnClickListener {
+            storedValue = currentValue
+            currentValue = 0
+        }
+        view.btnEqual.setOnClickListener {
+            currentValue += storedValue
+            updateResult()
+        }
+    }
+
+
+    private fun updateResult() {
+        view.tvResult.text = currentValue.toString()
+    }
+
+    private fun setNumberButtonListeners() {
+        view.btn0.setOnClickListener { onNumberClicked(0) }
+        view.btn1.setOnClickListener { onNumberClicked(1) }
+        view.btn2.setOnClickListener { onNumberClicked(2) }
+        view.btn3.setOnClickListener { onNumberClicked(3) }
+        view.btn4.setOnClickListener { onNumberClicked(4) }
+        view.btn5.setOnClickListener { onNumberClicked(5) }
+        view.btn6.setOnClickListener { onNumberClicked(6) }
+        view.btn7.setOnClickListener { onNumberClicked(7) }
+        view.btn8.setOnClickListener { onNumberClicked(8) }
+        view.btn9.setOnClickListener { onNumberClicked(9) }
+    }
+
+    private fun onNumberClicked(number: Int) {
+        currentValue = currentValue * 10 + number
+        updateResult()
+    }
+
 }
