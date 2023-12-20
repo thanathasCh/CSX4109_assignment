95b650b86e39f7e7ea5bb3020e185e9f6e51dee6
Wolf10212 <143413261+pavel10212@users.noreply.github.com>
2023-12-20 22:14:16 +0700

    xml done

--- app/src/main/java/com/csx4109/assignment/Assignment1Activity.kt
+++ app/src/main/java/com/csx4109/assignment/Assignment1Activity.kt
@@ -24,17 +24,60 @@ import com.csx4109.assignment.databinding.ActivityExampleAssignmentBinding
  * - When click `btnClear`, value on `tvResult` should return to 0
  */
 class Assignment1Activity : AppCompatActivity() {
-    private val view: ActivityAssignment1Binding by lazy { ActivityAssignment1Binding.inflate(layoutInflater) }
+    private val view: ActivityAssignment1Binding by lazy {
+        ActivityAssignment1Binding.inflate(
+            layoutInflater
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(view.root)
 
         view.btnPlus.setOnClickListener {
-            val newView = view.tvResult.text.toString().toInt()
-            view.tvResult.text = newView.toString()
+            val newView = view.tvResult.text.toString()
+            view.tvResult.text = newView.plus("+")
         }
-        view.btn0.setOnClickListener{
-            val val1 = int 1
+        view.btn0.setOnClickListener {
+            val val0 = 0
+            view.tvResult.text = val0.toString()
+        }
+        view.btn1.setOnClickListener {
+            val val1 = 1
+            view.tvResult.text = val1.toString()
+        }
+        view.btn2.setOnClickListener {
+            val val2 = 2
+            view.tvResult.text = val2.toString()
+        }
+        view.btn3.setOnClickListener {
+            val val3 = 3
+            view.tvResult.text = val3.toString()
+        }
+        view.btn4.setOnClickListener {
+            val val4 = 4
+            view.tvResult.text = val4.toString()
+        }
+        view.btn5.setOnClickListener {
+            val val5 = 5
+            view.tvResult.text = val5.toString()
+        }
+        view.btn6.setOnClickListener {
+            val val6 = 6
+            view.tvResult.text = val6.toString()
+        }
+        view.btn7.setOnClickListener {
+            val val7 = 7
+            view.tvResult.text = val7.toString()
+        }
+        view.btn8.setOnClickListener {
+            val val8 = 8
+            view.tvResult.text = val8.toString()
+        }
+        view.btn9.setOnClickListener {
+            val val9 = 9
+            view.tvResult.text = val9.toString()
+
         }
     }
 }
