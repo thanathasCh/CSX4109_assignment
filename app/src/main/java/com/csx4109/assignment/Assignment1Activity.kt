68eae2e4d6518a6d1b8550b24e7159eeed84afa2
653000000000 <153351513+653000000000@users.noreply.github.com>
2023-12-21 23:54:31 +0700

    Submit assg0+1
    :D

--- app/src/main/java/com/csx4109/assignment/Assignment1Activity.kt
+++ app/src/main/java/com/csx4109/assignment/Assignment1Activity.kt
@@ -2,6 +2,9 @@ package com.csx4109.assignment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 /**
  * Page Name: Basic Calculator
@@ -22,8 +25,78 @@ import android.os.Bundle
  * - When click `btnClear`, value on `tvResult` should return to 0
  */
 class Assignment1Activity : AppCompatActivity() {
+
+    private lateinit var tvResult: TextView
+    private var currentInput: String = ""
+    private var currentOperator: String? = null
+    private var result: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_assignment1)
+
+        tvResult = findViewById(R.id.tvResult)
+        setOnClickListeners()
+    }
+
+    private fun setOnClickListeners() {
+        val numberButtons = listOf<Button>(
+            findViewById(R.id.btn0),
+            findViewById(R.id.btn1),
+            findViewById(R.id.btn2),
+            findViewById(R.id.btn3),
+            findViewById(R.id.btn4),
+            findViewById(R.id.btn5),
+            findViewById(R.id.btn6),
+            findViewById(R.id.btn7),
+            findViewById(R.id.btn8),
+            findViewById(R.id.btn9)
+        )
+
+        for (button in numberButtons) {
+            button.setOnClickListener { onNumberButtonClick(it) }
+        }
+
+        findViewById<Button>(R.id.btnPlus).setOnClickListener { onOperatorButtonClick("+") }
+        findViewById<Button>(R.id.btnEqual).setOnClickListener { onEqualButtonClick() }
+        findViewById<Button>(R.id.btnClear).setOnClickListener { onClearButtonClick() }
+    }
+
+    private fun onNumberButtonClick(view: View) {
+        val number = (view as Button).text.toString()
+        currentInput += number
+        updateDisplay()
+    }
+
+    private fun onOperatorButtonClick(operator: String) {
+        if (currentInput.isNotEmpty()) {
+            currentOperator = operator
+            result = currentInput.toInt()
+            currentInput = ""
+        }
+    }
+
+    private fun onEqualButtonClick() {
+        if (currentInput.isNotEmpty() && currentOperator != null) {
+            val secondOperand = currentInput.toInt()
+            when (currentOperator) {
+                "+" -> result += secondOperand
+                // Add more cases for other operators if needed
+            }
+            currentOperator = null
+            currentInput = result.toString()
+            updateDisplay()
+        }
+    }
+
+    private fun onClearButtonClick() {
+        currentInput = ""
+        currentOperator = null
+        result = 0
+        updateDisplay()
+    }
+
+    private fun updateDisplay() {
+        tvResult.text = currentInput.takeIf { it.isNotEmpty() } ?: "0"
     }
 }
