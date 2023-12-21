25426a11a74c89d84dd6b65faddae8d8beee08f7
Lut-Lat-Aung <111869109+Lut-Lat-Aung@users.noreply.github.com>
2023-12-22 00:47:59 +0700

    6511163_Assignment_1_commit

--- app/src/main/java/com/csx4109/assignment/Assignment1Activity.kt
+++ app/src/main/java/com/csx4109/assignment/Assignment1Activity.kt
@@ -2,6 +2,8 @@ package com.csx4109.assignment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.csx4109.assignment.databinding.ActivityAssignment1Binding
 
 /**
  * Page Name: Basic Calculator
@@ -21,9 +23,52 @@ import android.os.Bundle
  * - After click `btnEqual` to calculate the result, you should be able to type more number for further calculation
  * - When click `btnClear`, value on `tvResult` should return to 0
  */
+
 class Assignment1Activity : AppCompatActivity() {
+    private var operator: String = ""
+    private val view: ActivityAssignment1Binding by lazy {ActivityAssignment1Binding.inflate(layoutInflater)}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_assignment1)
+        setContentView(view.root)
+
+        view.tvResult.text = "0"
+
+        val numberButtons = listOf<Button>(
+            view.btn0, view.btn1, view.btn2, view.btn3, view.btn4,
+            view.btn5, view.btn6, view.btn7, view.btn8, view.btn9
+        )//all the button together in the list
+
+        var theinput = ""
+        numberButtons.forEachIndexed { i, button -> // taking the desired button when click
+            button.setOnClickListener {
+                theinput += i.toString()
+                view.tvResult.text = theinput
+            }
+        }
+        view.btnPlus.setOnClickListener {//adding + operator
+            operator = "+"
+            theinput += operator
+            view.tvResult.text = theinput
+
+        }
+
+        view.btnEqual.setOnClickListener {//summing the two numbers
+
+            val parts = theinput.split("+")
+            var ans = parts.sumBy { it.toInt() }
+            view.tvResult.text = ans.toString()
+            theinput = ans.toString()
+
+
+        }
+
+        view.btnClear.setOnClickListener {//clearing all and showing zero
+            theinput = "0"
+            view.tvResult.text = theinput
+        }
     }
+
+
+
 }
