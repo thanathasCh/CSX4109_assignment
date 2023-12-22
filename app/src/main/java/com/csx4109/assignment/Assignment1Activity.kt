e30e226fca062d03e49e83cc69e92a727c86b434
thuyein96 <thuyein1996alpha@gmail.com>
2023-12-22 18:38:31 +0700

    Submission homework 1

--- app/src/main/java/com/csx4109/assignment/Assignment1Activity.kt
+++ app/src/main/java/com/csx4109/assignment/Assignment1Activity.kt
@@ -2,6 +2,7 @@ package com.csx4109.assignment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.csx4109.assignment.databinding.ActivityAssignment1Binding
 
 /**
  * Page Name: Basic Calculator
@@ -22,8 +23,62 @@ import android.os.Bundle
  * - When click `btnClear`, value on `tvResult` should return to 0
  */
 class Assignment1Activity : AppCompatActivity() {
+    private val view: ActivityAssignment1Binding by lazy {
+        ActivityAssignment1Binding.inflate(
+            layoutInflater
+        )
+    }
+    private var firstNumber: Int = 0
+    private var secondNumber: Int = 0
+    private var isSumPending = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_assignment1)
+        setContentView(view.root)
+
+        view.tvResult.text = "0"
+
+        view.btn0.setOnClickListener { addNumber("0") }
+        view.btn1.setOnClickListener { addNumber("1") }
+        view.btn2.setOnClickListener { addNumber("2") }
+        view.btn3.setOnClickListener { addNumber("3") }
+        view.btn4.setOnClickListener { addNumber("4") }
+        view.btn5.setOnClickListener { addNumber("5") }
+        view.btn6.setOnClickListener { addNumber("6") }
+        view.btn7.setOnClickListener { addNumber("7") }
+        view.btn8.setOnClickListener { addNumber("8") }
+        view.btn9.setOnClickListener { addNumber("9") }
+
+        view.btnPlus.setOnClickListener { performSum() }
+        view.btnEqual.setOnClickListener { calculateResult() }
+        view.btnClear.setOnClickListener { clear() }
+    }
+
+    private fun addNumber(number: String) {
+        val currentText = view.tvResult.text.toString()
+        view.tvResult.text = if (currentText == "0" || isSumPending) number else "$currentText$number"
+        isSumPending = false
+    }
+
+    private fun performSum() {
+        firstNumber = view.tvResult.text.toString().toInt()
+        isSumPending = true
+        view.tvResult.text = firstNumber.toString() + "+"
+    }
+
+    private fun calculateResult() {
+        var previousInput = view.tvResult.text.toString()
+        var secondNumber = previousInput.substringBefore("+")
+        view.tvResult.text = (firstNumber.toInt() + secondNumber.toInt()).toString()
+        isSumPending = false
+    }
+
+    private fun clear() {
+        view.tvResult.text = "0"
+        firstNumber = 0
+        secondNumber = 0
+        isSumPending = false
     }
 }
+
+
