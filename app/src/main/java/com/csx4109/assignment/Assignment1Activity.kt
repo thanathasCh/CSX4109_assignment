83fbe2dfbe0801f297083b2a3e8fda36869ebf48
Thanatuch <thanatuch@greenmoons.co.th>
2023-12-22 23:08:30 +0700

    add assinment2

--- app/src/main/java/com/csx4109/assignment/Assignment1Activity.kt
+++ app/src/main/java/com/csx4109/assignment/Assignment1Activity.kt
@@ -2,6 +2,7 @@ package com.csx4109.assignment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.csx4109.assignment.databinding.ActivityAssignment1Binding
 
 /**
  * Page Name: Basic Calculator
@@ -22,8 +23,64 @@ import android.os.Bundle
  * - When click `btnClear`, value on `tvResult` should return to 0
  */
 class Assignment1Activity : AppCompatActivity() {
+    private val view: ActivityAssignment1Binding by lazy { ActivityAssignment1Binding.inflate(layoutInflater) }
+    private var result = 0
+    private var isEqual = false
+    private var plusValue = 0
+    private var operationText = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_assignment1)
+        setContentView(view.root)
+
+        view.btnPlus.setOnClickListener {
+            handleOperationClick("+")
+        }
+
+        view.btnEqual.setOnClickListener {
+            result += plusValue
+            view.tvResult.text = result.toString()
+            operationText = ""
+            isEqual = true
+            plusValue = 0
+        }
+
+        view.btnClear.setOnClickListener {
+            result = 0
+            view.tvResult.text = "0"
+        }
+
+        val numberButtons = listOf(view.btn0, view.btn1, view.btn2, view.btn3, view.btn4, view.btn5, view.btn6, view.btn7, view.btn8, view.btn9)
+
+        numberButtons.forEach { button ->
+            button.setOnClickListener {
+                handleNumberClick(button.text.toString().toInt())
+            }
+        }
+    }
+
+    private fun handleOperationClick(operator: String) {
+        operationText = "$operator"
+        view.tvResult.text = view.tvResult.text.toString() + operationText
+    }
+
+    private fun handleNumberClick(number: Int) {
+        if (operationText.isEmpty()) {
+            if (view.tvResult.text == "0" || isEqual){
+                result = number
+                view.tvResult.text = number.toString()
+                isEqual = false
+            }else{
+                result = (result.toString() + number).toInt()
+                view.tvResult.text = view.tvResult.text.toString() + number
+            }
+        } else {
+            plusValue = if (plusValue == 0){
+                number
+            }else{
+                (plusValue.toString() + number.toString()).toInt()
+            }
+            view.tvResult.text = view.tvResult.text.toString() + number
+        }
     }
 }
