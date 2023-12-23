d2960e1b6ad4a4c3be112f01ec97c3934b96ccce
NyanLinHtet <136183847+GarrisXM@users.noreply.github.com>
2023-12-23 17:50:54 +0700

    Revert "Update test"
    This reverts commit 0158c27cbc7c470bedf4b231b93e468b6cd3a284.

--- app/src/androidTest/java/com/csx4109/assignment/ExampleAssignmentInstrumentedTest.kt
+++ app/src/androidTest/java/com/csx4109/assignment/ExampleAssignmentInstrumentedTest.kt
@@ -3,11 +3,11 @@
 //import androidx.test.core.app.ActivityScenario
 //import androidx.test.espresso.Espresso.onView
 //import androidx.test.espresso.action.ViewActions.click
-//import androidx.test.espresso.assertion.ViewAssertions.matches
 //import androidx.test.espresso.matcher.ViewMatchers.withText
 //import androidx.test.espresso.matcher.ViewMatchers.withId
 //import androidx.test.ext.junit.runners.AndroidJUnit4
 //import androidx.test.filters.LargeTest
+//import org.hamcrest.Matchers.allOf
 //import org.junit.Before
 //import org.junit.Test
 //import org.junit.runner.RunWith
@@ -24,8 +24,12 @@
 //
 //    @Test
 //    fun defaultValueShouldBe0() {
-//        onView(withId(R.id.tvCounter))
-//            .check(matches(withText("0")))
+//        onView(
+//            allOf(
+//                withId(R.id.tvCounter),
+//                withText("0")
+//            )
+//        )
 //    }
 //
 //    @Test
@@ -34,8 +38,12 @@
 //            onView(withId(R.id.btnPlus))
 //                .perform(click())
 //
-//            onView(withId(R.id.tvCounter))
-//                .check(matches(withText(i.toString())))
+//            onView(
+//                allOf(
+//                    withId(R.id.tvCounter),
+//                    withText(i.toString())
+//                )
+//            )
 //        }
 //    }
 //
@@ -45,8 +53,12 @@
 //            onView(withId(R.id.btnMinus))
 //                .perform(click())
 //
-//            onView(withId(R.id.tvCounter))
-//                .check(matches(withText(i.toString())))
+//            onView(
+//                allOf(
+//                    withId(R.id.tvCounter),
+//                    withText(i.toString())
+//                )
+//            )
 //        }
 //    }
 //}
