d9225d8ea7747e394e27d37dd9651c78c36d9ce0
Aung Cham Myae <86603047+acm1995@users.noreply.github.com>
2023-12-23 22:12:47 +0700

    assignment 1

--- app/src/main/java/com/csx4109/assignment/Assignment1Activity.kt
+++ app/src/main/java/com/csx4109/assignment/Assignment1Activity.kt
@@ -2,7 +2,8 @@ package com.csx4109.assignment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.widget.Button
+import com.csx4109.assignment.databinding.ActivityAssignment1Binding
 /**
  * Page Name: Basic Calculator
  *
@@ -22,8 +23,85 @@ import android.os.Bundle
  * - When click `btnClear`, value on `tvResult` should return to 0
  */
 class Assignment1Activity : AppCompatActivity() {
+
+    private lateinit var view: ActivityAssignment1Binding
+    private var currentInput: String = "0"
+    private var currentOperator: String? = null
+    private var operand1: Double = 0.0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_assignment1)
+        view = ActivityAssignment1Binding.inflate(layoutInflater)
+        setContentView(view.root)
+
+        // Set onClickListeners for number buttons
+        val numberButtons = listOf<Button>(
+            view.btn0, view.btn1, view.btn2, view.btn3, view.btn4,
+            view.btn5, view.btn6, view.btn7, view.btn8, view.btn9
+        )
+
+        for (button in numberButtons) {
+            button.setOnClickListener { onNumberClick(button.text.toString()) }
+        }
+
+        // Set onClickListener for operator buttons
+        val operatorButtons = listOf<Button>(
+            view.btnPlus
+            // Add more operator buttons as needed
+        )
+
+        for (button in operatorButtons) {
+            button.setOnClickListener { onOperatorClick(button.text.toString()) }
+        }
+
+        // Set onClickListeners for Equal and Clear buttons
+        view.btnEqual.setOnClickListener { onEqualClick() }
+        view.btnClear.setOnClickListener { onClearClick() }
+    }
+
+    private fun onNumberClick(number: String) {
+        if (currentInput == "0" || currentInput == "-0") {
+            currentInput = number
+        } else {
+            currentInput += number
+        }
+        updateDisplay()
+    }
+
+    private fun onOperatorClick(operator: String) {
+        if (currentOperator != null) {
+            performCalculation()
+        }
+        currentOperator = operator
+        operand1 = currentInput.toDouble()
+        currentInput = "0"
+    }
+
+    private fun onEqualClick() {
+        if (currentOperator != null) {
+            performCalculation()
+            currentOperator = null
+        }
+    }
+
+    private fun onClearClick() {
+        currentInput = "0"
+        currentOperator = null
+        operand1 = 0.0
+        updateDisplay()
+    }
+
+    private fun performCalculation() {
+        val operand2 = currentInput.toDouble()
+        when (currentOperator) {
+            "+" -> operand1 += operand2
+            // Add cases for other operators if needed
+        }
+        currentInput = operand1.toString()
+        updateDisplay()
+    }
+
+    private fun updateDisplay() {
+        view.tvResult.text = currentInput
     }
 }
