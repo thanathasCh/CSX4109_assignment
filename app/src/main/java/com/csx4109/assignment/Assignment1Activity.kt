d24e6e58f64b46a358150a950af81cac21080176
renattafr <u6338017@au.edu>
2023-12-23 23:53:48 +0700

    submit assignment 1

--- app/src/main/java/com/csx4109/assignment/Assignment1Activity.kt
+++ app/src/main/java/com/csx4109/assignment/Assignment1Activity.kt
@@ -2,6 +2,9 @@ package com.csx4109.assignment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.style.TtsSpan.DigitsBuilder
+import android.util.Log
+import com.csx4109.assignment.databinding.ActivityAssignment1Binding
 
 /**
  * Page Name: Basic Calculator
@@ -22,8 +25,59 @@ import android.os.Bundle
  * - When click `btnClear`, value on `tvResult` should return to 0
  */
 class Assignment1Activity : AppCompatActivity() {
+    private val view: ActivityAssignment1Binding by lazy { ActivityAssignment1Binding.inflate(layoutInflater) }
+    private var currentValue = 0
+    private var previousValue = 0
+    private var operator: Char? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_assignment1)
+        setContentView(view.root)
+
+        view.tvResult.text = "0"
+
+        val numberClickListener = { digit: String ->
+            val newValue = updatedResult(digit)
+            currentValue = currentValue * 10 + digit.toInt()
+            view.tvResult.text = newValue
+        }
+
+        view.btn0.setOnClickListener { numberClickListener("0") }
+        view.btn1.setOnClickListener { numberClickListener("1") }
+        view.btn2.setOnClickListener { numberClickListener("2") }
+        view.btn3.setOnClickListener { numberClickListener("3") }
+        view.btn4.setOnClickListener { numberClickListener("4") }
+        view.btn5.setOnClickListener { numberClickListener("5") }
+        view.btn6.setOnClickListener { numberClickListener("6") }
+        view.btn7.setOnClickListener { numberClickListener("7") }
+        view.btn8.setOnClickListener { numberClickListener("8") }
+        view.btn9.setOnClickListener { numberClickListener("9") }
+
+        view.btnPlus.setOnClickListener {
+            previousValue = view.tvResult.text.toString().toInt()
+            view.tvResult.text = view.tvResult.text.toString()+'+'
+            currentValue = 0
+        }
+
+        view.btnEqual.setOnClickListener {
+            Log.i("fr", currentValue.toString())
+            Log.i("fr", previousValue.toString())
+            view.tvResult.text = (previousValue + currentValue.toString().toInt()).toString()
+        }
+
+
+        view.btnClear.setOnClickListener {
+            currentValue = 0
+            operator = null
+            view.tvResult.text = "0"
+        }
+    }
+
+    private fun updatedResult(digit: String): String {
+        return if (view.tvResult.text == "0") {
+            digit
+        } else {
+            view.tvResult.text.toString() + digit
+        }
     }
 }
