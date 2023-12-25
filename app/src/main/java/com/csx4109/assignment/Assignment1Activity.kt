901a7501b717a404545f4968bf7f733111f0b680
thanathasCh <tamasama077@gmail.com>
2023-12-26 01:49:57 +0700

    Solution for assignment 0 and 1

--- app/src/main/java/com/csx4109/assignment/Assignment1Activity.kt
+++ app/src/main/java/com/csx4109/assignment/Assignment1Activity.kt
@@ -2,6 +2,7 @@ package com.csx4109.assignment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.csx4109.assignment.databinding.ActivityAssignment1Binding
 
 /**
  * Page Name: Basic Calculator
@@ -22,8 +23,54 @@ import android.os.Bundle
  * - When click `btnClear`, value on `tvResult` should return to 0
  */
 class Assignment1Activity : AppCompatActivity() {
+    private val view: ActivityAssignment1Binding by lazy {
+        ActivityAssignment1Binding.inflate(
+            layoutInflater
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_assignment1)
+        setContentView(view.root)
+
+        val buttons = listOf(
+            view.btn0,
+            view.btn1,
+            view.btn2,
+            view.btn3,
+            view.btn4,
+            view.btn5,
+            view.btn6,
+            view.btn7,
+            view.btn8,
+            view.btn9
+        )
+
+        buttons.forEachIndexed { index, button ->
+            button.setOnClickListener {
+                val oldText =
+                    if (view.tvResult.text.toString() == "0") "" else view.tvResult.text.toString()
+                val text = oldText + index.toString()
+                view.tvResult.text = text
+            }
+        }
+
+        view.btnPlus.setOnClickListener {
+            val text = view.tvResult.text.toString() + "+"
+            view.tvResult.text = text
+        }
+
+        view.btnEqual.setOnClickListener {
+            val text = view.tvResult.text.toString()
+            val result = text
+                .split("+")
+                .filter { it.isNotBlank() }
+                .sumOf { it.toInt() }
+            view.tvResult.text = result.toString()
+        }
+
+        view.btnClear.setOnClickListener {
+            view.tvResult.text = "0"
+        }
     }
 }
