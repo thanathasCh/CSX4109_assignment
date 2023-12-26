8a368bc595a550bbe628c9d08b629f5257fe0725
Aris823 <u6520152@au.edu>
2023-12-26 21:24:14 +0700

    submission_assigment1

--- app/src/main/java/com/csx4109/assignment/Assignment1Activity.kt
+++ app/src/main/java/com/csx4109/assignment/Assignment1Activity.kt
@@ -2,6 +2,11 @@ package com.csx4109.assignment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AbsListView
+import android.widget.Button
+import com.csx4109.assignment.databinding.ActivityAssignment1Binding
+import com.csx4109.assignment.databinding.ActivityMainBinding
 
 /**
  * Page Name: Basic Calculator
@@ -22,8 +27,78 @@ import android.os.Bundle
  * - When click `btnClear`, value on `tvResult` should return to 0
  */
 class Assignment1Activity : AppCompatActivity() {
+    private val view:ActivityAssignment1Binding by lazy { ActivityAssignment1Binding.inflate(layoutInflater) }
+    private var currentResult: Int = 0
+    private var currentInput: String = ""
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_assignment1)
+        setContentView(view.root)
+
+        updateResult()
+
+        view.btnClear.setOnClickListener {
+            view.tvResult.text = "0"
+            currentResult = 0
+            currentInput = ""
+            updateResult()
+        }
+
+        val numberButtons = arrayOf(
+            view.btn0, view.btn1, view.btn2, view.btn3, view.btn4,
+            view.btn5, view.btn6, view.btn7, view.btn8, view.btn9
+        )
+
+        for (button in numberButtons) {
+            button.setOnClickListener {
+                onNumberButtonClick((it as Button).text.toString())
+            }
+        }
+
+        view.btnPlus.setOnClickListener {
+            onOperatorButtonClick("+")
+        }
+
+        view.btnEqual.setOnClickListener {
+            calculateResult()
+        }
+    }
+
+    private fun onNumberButtonClick(number: String) {
+        currentInput += number
+        updateResult()
+    }
+    private fun updateResult() {
+        val resultText = if (currentInput.isNotEmpty()) currentInput else currentResult.toString()
+        view.tvResult.text = resultText
+    }
+    private fun onOperatorButtonClick(operator: String) {
+        if (currentInput.isNotEmpty()) {
+            currentResult = currentInput.toInt()
+            currentInput = currentInput + "+"
+            view.tvResult.text = currentInput
+        }
+    }
+
+    fun calculateSum(expression: String): Int {
+        val regex = Regex("""(\d+)\s*\+\s*(\d+)""")
+        val matchResult = regex.find(expression) ?: throw IllegalArgumentException("Invalid expression format (must be in the form 'number + number')")
+
+        val (num1, num2) = matchResult.destructured
+
+        val number1 = num1.toInt()
+        val number2 = num2.toInt()
+
+        return number1 + number2
+    }
+
+    fun calculateResult() {
+            currentResult = calculateSum(currentInput)
+            currentInput = currentResult.toString()
+            updateResult()
     }
 }
+
+
+
