0112fbb732e95942c67f34ea98191f9015d8565f
thanathasCh <tamasama077@gmail.com>
2023-12-26 23:00:23 +0700

    Add assignment 2 instruction

---
+++ app/src/main/java/com/csx4109/assignment/LocalVariables.kt
@@ -0,0 +1,148 @@
+package com.csx4109.assignment
+
+import com.csx4109.assignment.models.Game
+
+object LocalVariables {
+    val games = listOf(
+        Game(
+            "The Legend of Zelda: Breath of the Wild",
+            "Action-adventure",
+            "https://upload.wikimedia.org/wikipedia/en/c/c6/The_Legend_of_Zelda_Breath_of_the_Wild.jpg",
+            10,
+            "An open-world adventure with Link in the land of Hyrule."
+        ),
+        Game(
+            "The Witcher 3: Wild Hunt",
+            "Action RPG",
+            "https://upload.wikimedia.org/wikipedia/en/0/0c/Witcher_3_cover_art.jpg",
+            10,
+            "Geralt's epic journey in a rich fantasy world."
+        ),
+        Game(
+            "Red Dead Redemption 2",
+            "Action-adventure",
+            "https://upload.wikimedia.org/wikipedia/en/4/44/Red_Dead_Redemption_II.jpg",
+            10,
+            "An immersive western experience following Arthur Morgan's story."
+        ),
+        Game(
+            "Super Mario Odyssey",
+            "Platform",
+            "https://upload.wikimedia.org/wikipedia/en/8/8d/Super_Mario_Odyssey.jpg",
+            10,
+            "Mario's globe-trotting adventure to rescue Princess Peach."
+        ),
+        Game(
+            "God of War",
+            "Action-adventure",
+            "https://upload.wikimedia.org/wikipedia/en/a/a7/God_of_War_4_cover.jpg",
+            10,
+            "Kratos and Atreus' journey through Norse mythology."
+        ),
+        Game(
+            "Minecraft",
+            "Sandbox",
+            "https://upload.wikimedia.org/wikipedia/en/5/51/Minecraft_cover.png",
+            9,
+            "A blocky, creative sandbox game with endless possibilities."
+        ),
+        Game(
+            "Fortnite",
+            "Battle Royale",
+            "https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/bltdaa08bc17301576f/60dc27ce2e95e10f21f3193c/8e3054435647a98c29bd131600e394a887e22841.png",
+            8,
+            "A popular battle royale game with building mechanics."
+        ),
+        Game(
+            "Overwatch",
+            "First-person shooter",
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Overwatch_circle_logo.svg/500px-Overwatch_circle_logo.svg.png",
+            9,
+            "A team-based multiplayer shooter with diverse characters."
+        ),
+        Game(
+            "Grand Theft Auto V",
+            "Action-adventure",
+            "https://upload.wikimedia.org/wikipedia/en/a/a5/Grand_Theft_Auto_V.png",
+            10,
+            "A satirical take on modern society set in Los Santos."
+        ),
+        Game(
+            "Dark Souls III",
+            "Action RPG",
+            "https://upload.wikimedia.org/wikipedia/en/b/bb/Dark_souls_3_cover_art.jpg",
+            9,
+            "A challenging journey through a dark and atmospheric world."
+        ),
+        Game(
+            "Sekiro: Shadows Die Twice",
+            "Action-adventure",
+            "https://upload.wikimedia.org/wikipedia/en/6/6e/Sekiro_art.jpg",
+            9,
+            "A shinobi's quest for revenge in Sengoku period Japan."
+        ),
+        Game(
+            "Elden Ring",
+            "Action RPG",
+            "https://upload.wikimedia.org/wikipedia/en/b/b9/Elden_Ring_Box_art.jpg",
+            10,
+            "An open-world game with a rich narrative and challenging gameplay."
+        ),
+        Game(
+            "Cyberpunk 2077",
+            "Action RPG",
+            "https://upload.wikimedia.org/wikipedia/en/9/9f/Cyberpunk_2077_box_art.jpg",
+            7,
+            "A futuristic RPG set in the sprawling metropolis of Night City."
+        ),
+        Game(
+            "Among Us",
+            "Party",
+            "https://upload.wikimedia.org/wikipedia/en/9/9a/Among_Us_cover_art.jpg",
+            8,
+            "A game of teamwork and betrayal in space."
+        ),
+        Game(
+            "Animal Crossing: New Horizons",
+            "Simulation",
+            "https://upload.wikimedia.org/wikipedia/en/1/1f/Animal_Crossing_New_Horizons.jpg",
+            9,
+            "Create your own island paradise and live your dream life."
+        ),
+        Game(
+            "Doom Eternal",
+            "First-person shooter",
+            "https://upload.wikimedia.org/wikipedia/en/9/9d/Cover_Art_of_Doom_Eternal.png",
+            9,
+            "An intense and fast-paced shooter against demonic forces."
+        ),
+        Game(
+            "Final Fantasy VII Remake",
+            "JRPG",
+            "https://upload.wikimedia.org/wikipedia/en/c/ce/FFVIIRemake.png",
+            9,
+            "A modern retelling of the classic Final Fantasy VII story."
+        ),
+        Game(
+            "Apex Legends",
+            "Battle Royale",
+            "https://upload.wikimedia.org/wikipedia/en/d/db/Apex_legends_cover.jpg",
+            8,
+            "A character-driven battle royale with unique abilities."
+        ),
+        Game(
+            "League of Legends",
+            "MOBA",
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d8/League_of_Legends_2019_vector.svg/440px-League_of_Legends_2019_vector.svg.png",
+            9,
+            "A competitive team-based game with a vast array of champions."
+        ),
+        Game(
+            "Hades",
+            "Roguelike",
+            "https://upload.wikimedia.org/wikipedia/en/c/cc/Hades_cover_art.jpg",
+            9,
+            "A rogue-like dungeon crawler where you defy the god of the dead."
+        )
+    )
+}
