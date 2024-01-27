2c1904a2e9ad510c9472f0bc79c9d4a69c041f68
NyanLinHtet <136183847+GarrisXM@users.noreply.github.com>
2024-01-27 23:00:40 +0700

    6430031-ass2-submit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
+    implementation("io.coil-kt:coil:2.5.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.test.espresso:espresso-contrib:3.5.1")
     testImplementation("junit:junit:4.13.2")
