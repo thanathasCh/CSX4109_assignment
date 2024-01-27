88f1e2b7638a6bd69f5dd4406e934b7328fa3c22
SanzanaTahsin <u6138302@au.edu>
2024-01-27 18:11:42 +0700

    6138302-Assignment2

---
+++ app/src/main/java/com/csx4109/assignment/adapter/GGA.kt
@@ -0,0 +1,35 @@
+package com.csx4109.assignment.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.csx4109.assignment.databinding.GameGridItemBinding
+import com.csx4109.assignment.models.Game
+
+class GGA(private val context: Context, private val games: List<Game>) :
+    RecyclerView.Adapter<GGA.GameGridViewHolder>() {
+    class GameGridViewHolder(val view: GameGridItemBinding) : RecyclerView.ViewHolder(view.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GameGridViewHolder {
+        val binding =
+            GameGridItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return GameGridViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int = games.size
+
+    override fun onBindViewHolder(holder: GameGridViewHolder, position: Int) {
+        val game = games[position]
+
+        holder.view.ivGame.load(game.image)
+        holder.view.tvGameScore.text = game.rating.toString()
+
+        holder.view.root.setOnClickListener {
+            Toast.makeText(context, game.name, Toast.LENGTH_SHORT).show()
+        }
+    }
+}
