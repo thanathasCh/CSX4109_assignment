9375bd287fd1ce07db722ab3a4eaaa9fd9dca995
thuyein96 <thuyein1996alpha@gmail.com>
2024-01-27 12:50:09 +0700

    Assignment 2: Game List

--- app/src/main/java/com/csx4109/assignment/Assignment2Activity.kt
+++ app/src/main/java/com/csx4109/assignment/Assignment2Activity.kt
@@ -43,8 +43,32 @@ import com.csx4109.assignment.fragments.ListFragment
  * ***You can check video example in MS team***
  */
 class Assignment2Activity : AppCompatActivity() {
+    private val view: ActivityAssignment2Binding by lazy { ActivityAssignment2Binding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_assignment2)
+        setContentView(view.root)
+
+        if (savedInstanceState == null) {
+            changeFragment(ListFragment())
+        }
+
+        view.bnv.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.item_list -> changeFragment(ListFragment())
+                R.id.item_grid -> changeFragment(GridFragment())
+                else -> false
+            }
+        }
+    }
+
+    private fun changeFragment(fragment: Fragment): Boolean {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fcNavigation, fragment)
+            .addToBackStack(fragment::class.java.name)
+            .commit()
+
+        return true
     }
 }
---
+++ app/src/main/java/com/csx4109/assignment/adapter/GameListAdapter.kt
@@ -0,0 +1,32 @@
+package com.csx4109.assignment.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.csx4109.assignment.databinding.GameListItemBinding
+import com.csx4109.assignment.models.Game
+
+class GameListAdapter(private val games: List<Game>) :
+    RecyclerView.Adapter<GameListAdapter.GameListViewHolder>() {
+    class GameListViewHolder(val view: GameListItemBinding) : RecyclerView.ViewHolder(view.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GameListViewHolder {
+        val binding =
+            GameListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return GameListViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int = games.size
+
+    override fun onBindViewHolder(holder: GameListViewHolder, position: Int) {
+        val game = games[position]
+
+        holder.view.tvGameName.text = game.name
+        holder.view.tvGenre.text = game.genre
+        holder.view.tvGameDescription.text = game.description
+        holder.view.tvGameScore.text = game.rating.toString()
+        holder.view.ivGame.load(game.image)
+    }
+}
